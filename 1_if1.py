044eb0935482ab5b9d24f97b482190ad5e5d8409
Kazanchev Nikita <KazanchevNikita89@yandex.ru>
2023-02-21 08:27:00 +0300

    working out comments

--- 1_if1.py
+++ 1_if1.py
@@ -1,9 +1,9 @@
 def main():
-    age_request= int(input("введите ваш возрост: "))
-    if age_request >= 1 and age_request <= 7:
+    age = int(input("введите ваш возрост: "))
+    if 1 <= age <= 7:
         return "учится в детском саду"
        
-    elif age_request <= 17:
+    elif age <= 17:
         return "учится в школе"
        
     else:
--- 3_for.py
+++ 3_for.py
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
-    main()
+    main(phones)
